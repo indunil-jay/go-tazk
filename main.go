2b24befa079786a2611747ec453b5e9a16432c39
indunil jay <indunil.jayz@gmail.com>
2025-11-12 22:16:06 +0530

    feat : add health endpoint

--- main.go
+++ main.go
@@ -7,6 +7,11 @@ import (
 func main() {
     app := fiber.New()
 
+
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"status": "ok", "message": "Service is healthy"})
+	})
+
     fmt.Println("Starting server on :8080") 
     if err := app.Listen(":8080"); err != nil {
         fmt.Println("Error starting server:", err)
