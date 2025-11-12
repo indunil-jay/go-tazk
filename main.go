e12aad485a1970d403e0e02b5b90e6acd9ae402a
indunil jay <indunil.jayz@gmail.com>
2025-11-12 22:04:29 +0530

    feat : modify the server

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
-
 import (
-	"github.com/gofiber/fiber/v2"
+    "fmt"
+    "github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	app := fiber.New()
-	
-	app.Listen(":8080")
+    app := fiber.New()
+
+    fmt.Println("Starting server on :8080") 
+    if err := app.Listen(":8080"); err != nil {
+        fmt.Println("Error starting server:", err)
+    }
 }
+
